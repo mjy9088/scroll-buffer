96b822bc194b274c7d1999eedd77dc037a690f63
맹주영 <mjy9088@naver.com>
2019-01-30 01:17:10 +0900

    변수명 변경 & 불필요한 변수 삭제

--- js/scroll-buffer.js
+++ js/scroll-buffer.js
@@ -6,47 +6,46 @@ window.addEventListener('load', function ()
     body.style.overflow = 'hidden';
     var section = document.getElementsByTagName('SECTION')[0];
     section.style.position = 'fixed';
-    var height, animating = false, from = 0, to = 0, lt = Date.now(),
-        dr = 1, it = function (x) { return x * x; }, prev = 0;
+    var height, animating = false, idxFrom = 0, idxTo = 0, lastTime = Date.now(),
+        duration = 1, interpolator = function (x) { return x * x; };
     function draw()
     {
         animating = true;
-        var now = Date.now(), dt = now - lt;
-        if (dt > dr)
+        var now = Date.now(), deltaTime = now - lastTime;
+        if (deltaTime > duration)
         {
-            section.style.marginTop = -height * to + "px";
+            section.style.marginTop = -height * idxTo + "px";
             animating = false;
-            lt = now;
+            lastTime = now;
             return;
         }
-        section.style.marginTop = (from + (to - from) * it(dt / dr)) * -height + "px";
+        section.style.marginTop = (idxFrom + (idxTo - idxFrom) * interpolator(deltaTime / duration)) * -height + "px";
         window.requestAnimationFrame(draw);
     }
-    function resize()
+    function updateHeight()
     {
-        var w = window, d = document, e = d.documentElement;
-        height = w.innerHeight || e.clientHeight || body.clientHeight;
+        height = window.innerHeight || document.documentElement.clientHeight || body.clientHeight;
         if (!animating) window.requestAnimationFrame(draw);
     }
-    window.addEventListener('resize', resize);
+    window.addEventListener('resize', updateHeight);
     window.addEventListener('scroll', function ()
     {
         var tmp = window.pageYOffset || document.scrollTop,
-            now = Math.floor((tmp - (document.clientTop || 0)) / height);
-        if (isNaN(now)) now = 0;
-        if (prev != now) {
-            from = prev;
-            to = prev = now;
-            dr = 500;
+            idxNow = Math.floor((tmp - (document.clientTop || 0)) / height);
+        if (isNaN(idxNow)) idxNow = 0;
+        if (idxTo != idxNow) {
+            idxFrom = idxTo;
+            idxTo = idxNow;
+            duration = 500;
             if (animating)
             {
                 // TODO: 적당히 from 계산
                 return;
             }
-            it = function (x) { return x * x; }
-            lt = Date.now();
+            interpolator = function (x) { return x * x; }
+            lastTime = Date.now();
             window.requestAnimationFrame(draw);
         }
     });
-    resize();
+    updateHeight();
 });
